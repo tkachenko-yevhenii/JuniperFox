7ba4da3f32302c9f5e9cb35516790d9666262d61
tkachenko-yevhenii <tkachenkoyev@gmail.com>
2026-03-30 22:53:57 +0200

    updated items list

--- src/JuniperFox.Web/wwwroot/js/juniperfox.js
+++ src/JuniperFox.Web/wwwroot/js/juniperfox.js
@@ -10,3 +10,46 @@ window.JuniperFox = {
     }
   },
 };
+
+(function () {
+  if (!window.visualViewport || !document.body) {
+    return;
+  }
+
+  var KEYBOARD_CLASS = 'jf-keyboard-open';
+  var KEYBOARD_MIN_DIFF = 120;
+
+  function isEditableElement(element) {
+    if (!element) {
+      return false;
+    }
+
+    var tag = element.tagName;
+    if (tag === 'INPUT' || tag === 'TEXTAREA' || tag === 'SELECT') {
+      return true;
+    }
+
+    return element.isContentEditable === true;
+  }
+
+  function updateKeyboardState() {
+    var activeElement = document.activeElement;
+    var isEditing = isEditableElement(activeElement);
+    var viewportHeight = window.visualViewport.height;
+    var heightDiff = window.innerHeight - viewportHeight;
+    var isKeyboardOpen = isEditing && heightDiff > KEYBOARD_MIN_DIFF;
+
+    document.body.classList.toggle(KEYBOARD_CLASS, isKeyboardOpen);
+  }
+
+  window.visualViewport.addEventListener('resize', updateKeyboardState);
+  window.addEventListener('focusin', updateKeyboardState);
+  window.addEventListener('focusout', function () {
+    window.setTimeout(updateKeyboardState, 0);
+  });
+  window.addEventListener('orientationchange', function () {
+    window.setTimeout(updateKeyboardState, 100);
+  });
+
+  updateKeyboardState();
+})();
